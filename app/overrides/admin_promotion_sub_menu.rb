b275d1222eaa954c22f0301b61bf8909f5224ecc
Rene Schneider <renews@oqvestir.com.br>
2015-06-12 16:07:58 -0300

    Adiciona no Admin o link.

---
+++ app/overrides/admin_promotion_sub_menu.rb
@@ -0,0 +1,5 @@
+Deface::Override.new(:virtual_path => "spree/admin/shared/sub_menu/_promotion",
+                     :name => "admin_promotion_sub_menu_affiliates",
+                     :insert_top => "[data-hook='admin_promotion_sub_tabs']",
+                     :text => "<%= tab :affiliates, url: 'affiliates', icon: 'icon-th-large' %>",
+                     :disabled => false)
